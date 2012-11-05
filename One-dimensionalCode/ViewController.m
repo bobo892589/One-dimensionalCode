49da04b5f31642f94bc075dd4bf5d4e08b3371e3
bobo <540844924@qq.com>
2012-11-05 15:59:12 +0800

    add

--- One-dimensionalCode/ViewController.m
+++ One-dimensionalCode/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "UIImage-NKDBarcode.h"
+#import "NKDCode128Barcode.h"
 
 @interface ViewController ()
+@property (retain, nonatomic) IBOutlet UITextField *barcodeTextField;
+@property (retain, nonatomic) IBOutlet UITableView *barcodeMethod;
+@property (retain, nonatomic) IBOutlet UIImageView *barcodeImageView;
 
 @end
 
@@ -26,4 +31,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)dealloc {
+    [_barcodeTextField release];
+    [_barcodeMethod release];
+    [_barcodeImageView release];
+    [super dealloc];
+}
+- (IBAction)onCreateBarCodeImage:(id)sender
+{
+    if (!self.barcodeTextField.text || [self.barcodeTextField.text isEqualToString:@""]) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"错误" message:@"编码字符不为空" delegate:nil cancelButtonTitle:@"确认" otherButtonTitles: nil];
+        [alertView show];
+        return ;
+    }
+    NKDCode128Barcode *barcode = [[NKDCode128Barcode alloc] initWithContent:self.barcodeTextField.text printsCaption:YES];
+    UIImage *image = [UIImage imageFromBarcode:barcode];
+    self.barcodeImageView.image = image;
+}
 @end
